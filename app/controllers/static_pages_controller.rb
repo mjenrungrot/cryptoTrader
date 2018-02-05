6d0a54d50cfb95027d6527f78e0aa5f5217bb875
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-02-05 11:57:26 -0800

    Add Bitcoin price Tracker

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,18 @@
+require 'net/http'
+require 'json'
+
 class StaticPagesController < ApplicationController
   def home
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    uri = URI(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(uri.path, {'Content-Type' => 'application/json'})
+    response = http.request(request)
+    
+    @responseValue = JSON.parse(response.body)
+    p @responseValue
   end
 
   def help
