f60ed01f5787e0fcf401542dfc66caa187eafa86
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-02-06 01:30:38 -0800

    Add Login prototype

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  # sessions 
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # signup
   get '/signup',		to: 'users#new'
   post '/signup',   to: 'users#create'
 		
+  # static pages
   get '/help',			to: 'static_pages#help'
   get '/about',			to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'static_pages#home'
 
   # routes for /users/<id>
   resources :users
