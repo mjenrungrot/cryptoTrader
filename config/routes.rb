4e75d48394274f6a782a8c758eed969bfdfdbac4
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-02-06 00:32:06 -0800

    Finish signup page + testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/signup',		to: 'users#new'
+  post '/signup',   to: 'users#create'
 		
   get '/help',			to: 'static_pages#help'
   get '/about',			to: 'static_pages#about'
